b84f2011cda6110871a860931a12a8a15a4b6eeb
Yuriy <yuriy@harmony.one>
2024-04-09 04:29:01 +0300

    Update config.sh

--- packages/contracts-bedrock/scripts/getting-started/config.sh
+++ packages/contracts-bedrock/scripts/getting-started/config.sh
@@ -32,7 +32,7 @@ config=$(cat << EOL
 {
   "l1StartingBlockTag": "$blockhash",
 
-  "l1ChainID": 1666600000,
+  "l1ChainID": 1666600001,
   "l2ChainID": 42069,
   "l2BlockTime": 2,
   "l1BlockTime": 2,
